d9bafe875b075f4866b1fdcc5728f3090e774835
Yossi Arye <aryeyossi@gmail.com>
2026-04-30 14:18:20 +0300

    js variables and controle flow

---
+++ 02-javascript/01-javascript-basics/02-control-flow/script.js
@@ -0,0 +1,15 @@
+let userRole = "viewer"
+
+const isUserEditor = userRole === "editor"
+const isUserViewer = userRole === "viewer"
+
+console.log("isUserEditor:", isUserEditor)
+console.log("isUserViewer:", isUserViewer)
+
+if (isUserEditor) {
+  console.log("You can edit this movie")
+} else if (isUserViewer) {
+  console.log("You can view this movie")
+} else {
+  console.log("You don't have access")
+}
